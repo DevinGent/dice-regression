f5fb7f4b8da40dde6d20963e7a639d34537c0c22
Devin Gent <devgent@yahoo.com>
2023-08-04 10:16:44 -0400

    Created a folder to store plot images and finished the regression analysis of AQI based on rolls per test.

--- dice_regression.py
+++ dice_regression.py
@@ -16,6 +16,7 @@ fig, axes = plt.subplots(2, 3, figsize=(14, 6), sharex=True)
 for ax,col in zip(axes.flatten(),df.columns):
     sns.scatterplot(data=df, x='Rolls per Test', y=col, ax=ax)
 plt.tight_layout()
+plt.savefig('images/rolls-per-test.png')
 plt.show()
 
 # We now repeat with the Number of Tests as the x axis.
@@ -23,6 +24,7 @@ fig, axes = plt.subplots(2, 3, figsize=(14, 6), sharex=True)
 for ax,col in zip(axes.flatten(),df.columns):
     sns.scatterplot(data=df, x='Number of Tests', y=col, ax=ax)
 plt.tight_layout()
+plt.savefig('images/number-of-tests.png')
 plt.show()
 
 # It is clear that the size of each test is more closely correlated with results closer to ~16%, and generally precise results.
@@ -50,55 +52,80 @@ plt.legend()
 plt.show()
 # The training data looks good.  Let us start regression analysis.
 ########################################################################
-# First let us try an exponential model.
+# First let us try polynomial regression.  It looks like a linear or quadratic function would be unlikely to fit
+# the curve, so we will try a third degree polynomial: y=ax^3+bx^2+cx+d with a,b,c,d real numbers.
+
+cubic_fit=np.polynomial.Polynomial.fit(train_df['Rolls per Test'],train_df['IQR'],3).convert()
+print(cubic_fit)
+
+plt.figure(figsize=(8,6))
+sns.scatterplot(data=train_df, x='Rolls per Test', y='IQR', label='Actual (Training)')
+sns.lineplot(x=train_df['Rolls per Test'],y=cubic_fit(train_df['Rolls per Test']), color='C1', label='Predicted')
+plt.title('Cubic model')
+plt.legend()
+plt.show()
+
+print('For the cubic model:')
+print('The Coefficient of Determination on the training data is', r2_score(train_df['IQR'],cubic_fit(train_df['Rolls per Test']))) 
+
+# Let us see how the model works for the testing data.
+plt.figure(figsize=(8,6))
+sns.scatterplot(data=test_df, x='Rolls per Test', y='IQR',label='Actual (Testing)')
+sns.lineplot(x=test_df['Rolls per Test'],y=cubic_fit(test_df['Rolls per Test']), color='C1', label='Predicted')
+plt.title('Cubic model')
+plt.legend()
+plt.show()
+print('The Coefficient of Determination on the testing data is', r2_score(test_df['IQR'],cubic_fit(test_df['Rolls per Test'])))
+# Note that the R^2 value is fairly good, but it looks like there might be overfitting based on the graph. 
+# Can we do better?  Let us consider a different kind of model.
+########################################################################
+# Next let us try an exponential model.
 # If x is the number of rolls per test, and y is the corresponding IQR, we should try to fit the model y=a(e^(kx)) where
 # a and k are constants.
 # taking a logarithm on each side we obtain log(y)=log(a)+kx.
 # If we perform linear regression on the pair (x,log(y)) we should obtain a slope (to approximate k) 
 # and an intercept (to approximate log(a)).
-fit = np.polyfit(train_df['Rolls per Test'],np.log(train_df['IQR']),1)
-print(fit)
+exp_fit = np.polynomial.Polynomial.fit(train_df['Rolls per Test'],np.log(train_df['IQR']),1).convert()
+print(exp_fit)
 
-def expmodel(x):
+def exp_model(x):
     """Returns the y value for a given x input in our exponential model"""
     # y=a(e^(kx))
-    # Here k=fit[0] and a=exp(fit[1]) (since log(a)=fit[1])  
-    return np.exp(fit[1])*np.exp(x*fit[0])
+    # Here k=fit[1] and a=exp(fit[0]) (since log(a)=fit[0])  
+    return np.exp(exp_fit.coef[0])*np.exp(x*exp_fit.coef[1])
 
 plt.figure(figsize=(8,6))
 sns.scatterplot(data=train_df, x='Rolls per Test', y='IQR', label='Actual (Training)')
-sns.lineplot(x=train_df['Rolls per Test'],y=expmodel(train_df['Rolls per Test']), color='C1', label='Predicted')
+sns.lineplot(x=train_df['Rolls per Test'],y=exp_model(train_df['Rolls per Test']), color='C1', label='Predicted')
 plt.title('Exponential model')
 plt.legend()
 plt.show()
 
 print('For the exponential model:')
-print('The Coefficient of Determination on the training data is', r2_score(train_df['IQR'],expmodel(train_df['Rolls per Test']))) 
+print('The Coefficient of Determination on the training data is', r2_score(train_df['IQR'],exp_model(train_df['Rolls per Test']))) 
 
 # Let us see how the model works for the testing data.
 plt.figure(figsize=(8,6))
 sns.scatterplot(data=test_df, x='Rolls per Test', y='IQR',label='Actual (Testing)')
-sns.lineplot(x=test_df['Rolls per Test'],y=expmodel(test_df['Rolls per Test']), color='C1', label='Predicted')
+sns.lineplot(x=test_df['Rolls per Test'],y=exp_model(test_df['Rolls per Test']), color='C1', label='Predicted')
 plt.title('Exponential model')
 plt.legend()
 plt.show()
 
-print('The Coefficient of Determination on the testing data is', r2_score(test_df['IQR'],expmodel(test_df['Rolls per Test']))) 
-
-# Can we do better?  Let us consider a different kind of model.
+print('The Coefficient of Determination on the testing data is', r2_score(test_df['IQR'],exp_model(test_df['Rolls per Test']))) 
 
 ##########################################################
 # Let us try a power model of the form y=k(x^a) where k and a are real constants.
 # Applying log to each side, we have log(y)=log(k)+alog(x).
 # We can perform regression on the (input,output) pair (log(x),log(y)) to obtain a slope a and intercept log(k).
-fit = np.polyfit(np.log(train_df['Rolls per Test']),np.log(train_df['IQR']),1)
-print(fit)
+power_fit = np.polynomial.Polynomial.fit(np.log(train_df['Rolls per Test']),np.log(train_df['IQR']),1).convert()
+print(power_fit)
 
 def power_model(x):
     """Returns the y value for a given x input in our power model"""
     # y=k(x^a)
-    # Here a=fit[0] and k=exp(fit[1]) (since log(k)=fit[1])  
-    return np.exp(fit[1])*(x**fit[0])
+    # Here a=fit[1] and k=exp(fit[0]) (since log(k)=fit[0])  
+    return np.exp(power_fit.coef[0])*(x**power_fit.coef[1])
 
 plt.figure(figsize=(8,6))
 sns.scatterplot(data=train_df, x='Rolls per Test', y='IQR', label='Actual (Training)')
@@ -118,3 +145,16 @@ plt.legend()
 plt.show()
 
 print('The Coefficient of Determination on the testing data is', r2_score(test_df['IQR'],power_model(test_df['Rolls per Test']))) 
+# This looks, both from the R^2 score and the graph, like the most accurate choice of regression for the dataset.
+# Let us save a comparison of the different regression curves.
+
+plt.figure(figsize=(8,6))
+sns.set_style('darkgrid')
+sns.scatterplot(data=test_df, x='Rolls per Test', y='IQR',label='Actual', alpha=.75)
+sns.lineplot(x=test_df['Rolls per Test'],y=cubic_fit(test_df['Rolls per Test']), color='C1', label='Cubic', linewidth=2)
+sns.lineplot(x=test_df['Rolls per Test'],y=exp_model(test_df['Rolls per Test']), color='C2', label='Exponential', linewidth=2)
+sns.lineplot(x=test_df['Rolls per Test'],y=power_model(test_df['Rolls per Test']), color='C3', label='Power', linewidth=2)
+plt.title('Comparing models')
+plt.legend()
+plt.savefig('images/comparing-models.png')
+plt.show()
